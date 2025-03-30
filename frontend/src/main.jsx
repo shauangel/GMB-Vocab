fe127e5059a1abbf08658518a88aa700eeebe836
hazelhung <yoshiko.wish@gmail.com>
2025-03-30 12:10:01 -0400

    暫時儲存的修改

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import VocabApp from './App.jsx';   // 👈 確認這行是 VocabApp，不是 App
+import './index.css';
+
 
 console.log("main loaded");
 
